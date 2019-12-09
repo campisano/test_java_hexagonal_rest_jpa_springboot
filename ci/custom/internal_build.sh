35b4ac4f815452bd71a38c440ad4691c5c93e26b
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-09 01:15:50 -0300

    fix:        test ci scripts

--- ci/custom/internal_build.sh
+++ ci/custom/internal_build.sh
@@ -8,12 +8,12 @@ apt-get -qq -y clean
 
 MVN_VER="3.6.2"
 URL="http://www-eu.apache.org/dist/maven/maven-3/${MVN_VER}/binaries/apache-maven-${MVN_VER}-bin.tar.gz"
-curl -C - -kLO "${URL}"
+curl -s -C - -kLO "${URL}"
 tar -C /usr/local -xzf "apache-maven-${MVN_VER}-bin.tar.gz"
 rm -f "apache-maven-${MVN_VER}-bin.tar.gz"
 
 export PATH="/usr/local/apache-maven-${MVN_VER}/bin:$PATH"
-mvn -B -ntp clean test package spring-boot:repackage
+mvn -B -q -ntp clean test package spring-boot:repackage
 
 PACKAGE="$(mvn -B -q help:evaluate -Dexpression=project.build.finalName -DforceStdout).jar"
 FOLDER="$(mvn -B -q help:evaluate -Dexpression=project.build.directory -DforceStdout)"

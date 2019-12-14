cb87e0f9f2ce0ae223c412d3d0672b1c05c7099c
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-14 01:26:58 -0300

    feature:    improve CI scripts

--- ci/custom/get_project_name.sh
+++ ci/custom/get_project_name.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-type -P mvn || sudo ./ci/custom/install_maven.sh
+type -P mvn || sudo ./ci/custom/install_maven.sh &>/dev/null
 
 NAME=$(mvn -B -q help:evaluate -Dexpression=project.name -DforceStdout)
 echo "${NAME}"

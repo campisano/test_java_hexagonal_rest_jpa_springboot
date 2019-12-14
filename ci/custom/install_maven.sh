df6d0f02499f4b42815abaa47f60d956656eb584
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-14 19:34:35 -0300

    feature:    improve CI scripts

--- ci/custom/install_maven.sh
+++ ci/custom/install_maven.sh
@@ -3,7 +3,8 @@
 set -x -o errexit -o nounset -o pipefail
 
 MVN_VER="3.6.2"
-URL="http://www-eu.apache.org/dist/maven/maven-3/${MVN_VER}/binaries/apache-maven-${MVN_VER}-bin.tar.gz"
+URL="https://archive.apache.org/dist/maven/maven-3/${MVN_VER}/binaries/apache-maven-${MVN_VER}-bin.tar.gz"
+
 curl -s -C - -kLO "${URL}"
 tar -C /usr/local -xzf "apache-maven-${MVN_VER}-bin.tar.gz"
 rm -f "apache-maven-${MVN_VER}-bin.tar.gz"

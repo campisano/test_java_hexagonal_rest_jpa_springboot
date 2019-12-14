522b9fbdd06ce6dcaaa0bf42b42f7781780bc31d
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-14 19:14:18 -0300

    feature:    improve CI scripts

--- ci/custom/ci_requisites.sh
+++ ci/custom/ci_requisites.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-if type -P xmlstarlet &>/dev/null
+if ! type -P xmlstarlet &>/dev/null
 then
     apt-get -qq -y update
     apt-get -qq -y install xmlstarlet > /dev/null

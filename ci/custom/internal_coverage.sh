5d0964a8f2af767f141e21792f83b9455e27e587
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-31 11:55:58 -0300

    fix:        remove unecessary code from internal_coverage.sh after use jacoco

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -5,10 +5,7 @@ set -x -o errexit -o nounset -o pipefail
 TOKEN="$1"
 
 apt-get -qq -y update
-apt-get -qq -y install curl tar gzip git > /dev/null
+apt-get -qq -y install curl git > /dev/null
 apt-get -qq -y clean
-
-./ci/custom/install_maven.sh
-mvn -B -ntp cobertura:cobertura
 
 bash <(curl -s https://codecov.io/bash) -t "${TOKEN}"

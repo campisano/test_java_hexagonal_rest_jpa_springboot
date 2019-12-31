ddcb929f72bb0e74e0637cbada25860260330adf
riccardo.campisano <riccardo.campisano@gmail.com>
2019-12-31 03:54:19 -0300

    fix:        fix coverage

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -5,7 +5,7 @@ set -x -o errexit -o nounset -o pipefail
 TOKEN="$1"
 
 apt-get -qq -y update
-apt-get -qq -y install curl tar gzip > /dev/null
+apt-get -qq -y install curl tar gzip git > /dev/null
 apt-get -qq -y clean
 
 ./ci/custom/install_maven.sh

55ff4168193a16d2761aae7b91407dfe5f353617
riccardo.campisano <riccardo.campisano@gmail.com>
2020-01-03 01:20:34 -0300

    feature:    add tests

--- src/main/java/org/example/test_java_rest_jpa/application/BookDto.java
+++ src/main/java/org/example/test_java_rest_jpa/application/BookDto.java
@@ -23,31 +23,15 @@ public class BookDto {
         return isbn;
     }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }

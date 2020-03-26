04d4b509e6699c539b46de0ba16e57fcd3eb092d
riccardo.campisano <riccardo.campisano@gmail.com>
2020-03-26 04:07:41 -0300

    feature:    get project name by pom artifactId

--- ci/custom/get_project_name.sh
+++ ci/custom/get_project_name.sh
@@ -2,6 +2,6 @@
 
 set -o errexit -o nounset -o pipefail
 
-NAME=$(xmlstarlet sel -N mvn=http://maven.apache.org/POM/4.0.0 -t -v mvn:project/mvn:name pom.xml)
+NAME=$(xmlstarlet sel -N mvn=http://maven.apache.org/POM/4.0.0 -t -v mvn:project/mvn:artifactId pom.xml)
 
 echo "${NAME}"
